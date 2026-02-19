075d048df674824d5534b9bccdf12bd10cabca20
sobbingtofu <kukubasak@naver.com>
2026-02-19 19:07:24 +0900

    first commit

---
+++ src/styledComponentsRegistry.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, {useState} from "react";
+import {useServerInsertedHTML} from "next/navigation";
+import {ServerStyleSheet, StyleSheetManager} from "styled-components";
+
+export default function StyledComponentsRegistry({children}: {children: React.ReactNode}) {
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  // 서버 사이드 렌더링 시 스타일을 주입
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== "undefined") return <>{children}</>;
+
+  return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
+}
